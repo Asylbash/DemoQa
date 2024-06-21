0a33ce4bb5b585ca62768de49b33aa9f867721ac
ZHYLDYZ ADYLCHAEVA <165602940+Asylbash@users.noreply.github.com>
2024-06-21 13:46:57 +0200

    Update ProgressBarPage.java

--- src/main/java/com/demoqa/pages_raznye_stranicy_get/ProgressBarPage.java
+++ src/main/java/com/demoqa/pages_raznye_stranicy_get/ProgressBarPage.java
@@ -1,7 +1,7 @@
 package com.demoqa.pages_raznye_stranicy_get;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;;
+import org.openqa.selenium.support.FindBy;
 
 public class ProgressBarPage extends BasePage{
     @FindBy(id = "startStopButton")
